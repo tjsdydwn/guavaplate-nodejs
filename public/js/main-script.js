37fa32fd3fa977b87dd1122acfe1dfb4e8e36142
todak_sun <tjsdydwn@gmail.com>
2019-10-25 09:42:15 +0900

    메인 스크립트 및 페이지 추가

--- public/js/main-script.js
+++ public/js/main-script.js
@@ -1,3 +1,64 @@
-const pageInfo = {
-    _mainurl: `${location.host}`
-};
+$(function() {
+    const $btn = {
+        login: $('#btn-login'),
+        logout: $('#btn-logout'),
+        regist: $('#btn-regist'),
+        myinfo: $('#btn-myinfo'),
+        loginUser: $('#btn-login-user')
+    }
+
+    const $modalLogin = {
+        body: $('#modal-login'),
+        id: $('#id-login'),
+        pwd: $('#pwd-login'),
+        reserveLogin : $('#reserve-login')
+    }
+
+    const handler = {
+        loginClick(e) {
+            e.preventDefault();
+            $modalLogin.body.modal('show');
+        },
+        logoutClick(e) {
+
+        },
+        registClick(e) {
+
+        },
+        myinfoClick(e) {
+
+        },
+        loginUserClick(e) {
+            e.preventDefault();
+            console.log($modalLogin.id.val(), $modalLogin.pwd.val(), $modalLogin.reserveLogin.checked);
+        }
+    }
+
+    $btn.login.on({ 'click': handler.loginClick });
+    $btn.logout.on({ 'click': handler.logoutClick });
+    $btn.regist.on({ 'click': handler.registClick });
+    $btn.myinfo.on({ 'click': handler.myinfoClick });
+
+    $btn.loginUser.on({ 'click': handler.loginUserClick });
+})
+
+
+
+//ajax 요청
+function getReviewPage(data) {
+    return $.ajax({
+        type: 'GET',
+        url: `${location.host}/review`,
+        dataType: '',
+        data: data
+    })
+}
+
+function getWritePage(data) {
+    return $.ajax({
+        type: 'GET',
+        url: `${location.host}/write`,
+        dataType: '',
+        data: data
+    })
+}
